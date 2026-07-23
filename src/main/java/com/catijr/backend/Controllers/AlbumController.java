6d4ae2b3b9b88fd6490a801ceb124f2981ff01bb
Hugo Aparecido Gregorio de Almeida <120059026+HugoAparecido@users.noreply.github.com>
2026-07-23 07:53:39 -0300

    feat(backend): adiciona configuração de CORS em AlbumController, ArtistController e PlaylistController
    Habilitação de CORS: Aplica a anotação @CrossOrigin(origins = "http://localhost:5173", maxAge = 3600) nas classes de controle (AlbumController, ArtistController e PlaylistController) do Spring Boot.
    
    Comunicação Frontend-Backend: Garante a liberação de requisições vindas da porta padrão de desenvolvimento do Vite/React (http://localhost:5173), prevenindo erros de bloqueio por política de mesma origem (CORS policy).

--- src/main/java/com/catijr/backend/Controllers/AlbumController.java
+++ src/main/java/com/catijr/backend/Controllers/AlbumController.java
@@ -4,6 +4,7 @@ import com.catijr.backend.DTOs.Music.GetMusicDTO;
 import com.catijr.backend.Services.AlbumService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/album/")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
 public class AlbumController {
 
     private final AlbumService albumService;
